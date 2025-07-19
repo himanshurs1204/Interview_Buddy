97739d151267e91daa81a6f4f767cbcb42519c4a
himanshurs1204 <117307036+himanshurs1204@users.noreply.github.com>
2025-07-19 13:41:22 +0530

    Update apiPath.js

--- client/src/utils/apiPath.js
+++ client/src/utils/apiPath.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://interview-buddy-jcz5.onrender.com";
 export const API_PATHS = {
     AUTH: {
         REGISTER: "/api/auth/register", // Signup
