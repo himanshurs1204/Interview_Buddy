6cec0dc806fa7da0ded814499ce144c0f15eef61
Himanshu Suryawanshi <117307036+himanshurs1204@users.noreply.github.com>
2026-09-18 18:34:19 +0530

    Update BASE_URL to production endpoint

--- client/src/utils/apiPath.js
+++ client/src/utils/apiPath.js
@@ -1,5 +1,5 @@
 // require('dotenv').config();
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://interview-buddy-jcz5.onrender.com/";
 export const API_PATHS = {
     AUTH: {
         REGISTER: "/api/auth/register", // Signup
